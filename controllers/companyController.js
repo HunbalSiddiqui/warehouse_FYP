1fdb6e88122542845d326d1617c7fd3fdfbc39da
MuhammadHunbalSiddiqui-PivotalWorks <hunbal.siddiqui@pivotalworks.io>
2021-12-31 01:59:09 +0500

    Update company.

--- controllers/companyController.js
+++ controllers/companyController.js
@@ -56,9 +56,38 @@ exports.getCompany = async (req, res, next) => {
             return res.status(404).json({
                 status: "error",
                 success: false,
-                error: "Company not found",
+                error: "Company does not exist.",
             });
         }
+        return res.status(200).json({
+            success: true,
+            status: "success",
+            data: {
+                company
+            },
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: "error",
+            success: false,
+            error: error.message,
+        });
+    }
+}
+
+exports.updateCompany = async (req, res, next) => {
+    try {
+        var company = await Company.findOne({ _id: req.params.id });
+        if (!company) {
+            return res.status(404).json({
+                status: "error",
+                success: false,
+                error: "Company does not exist.",
+            });
+        }
+        company = await Company.findOneAndUpdate({ _id: req.params.id }, req.body, {
+            new: true,
+        });
         return res.status(200).json({
             success: true,
             status: "success",
--- routes/companyRoute.js
+++ routes/companyRoute.js
@@ -1,10 +1,11 @@
 const express = require("express")
-const { createCompany, getCompanies, getCompany } = require("../controllers/companyController")
+const { createCompany, getCompanies, getCompany, updateCompany } = require("../controllers/companyController")
 
 const router = express.Router()
 
 router.post("/", createCompany)
 router.get("/", getCompanies)
 router.get("/:id", getCompany)
+router.patch("/:id", updateCompany);
 
 module.exports = router
