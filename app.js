183e4e6d7e486f6da00917c96f56dbea2ba87472
Hunbal Siddiqui <hunbal.dha@gmail.com>
2022-03-08 01:16:50 +0500

    cors fix 2

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const cityRouter = require("./routes/cityRoute")
 const roleRouter = require("./routes/roleRoute")
 const app = express();
 //implementing cors
-// app.use(cors({ origin: true, credentials: true }))
-app.use(cors())
+app.use(cors({ origin: true, credentials: true }))
+// app.use(cors())
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter)
